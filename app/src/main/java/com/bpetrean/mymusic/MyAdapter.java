d5625632dd260ed9b7bce3b89bea5ebf5e09149e
bpetrean <petrean.beaatrice@yahoo.com>
2019-07-16 13:58:46 +0300

    modified

--- app/src/main/java/com/bpetrean/mymusic/MyAdapter.java
+++ app/src/main/java/com/bpetrean/mymusic/MyAdapter.java
@@ -1,41 +1,26 @@
 package com.bpetrean.mymusic;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+import com.bumptech.glide.Glide;
 
-    private String[] mDataset;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private List<Song> songs;
     private OnSongClickedListener listener;
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-
-        public View view;
-        public TextView textView;
-
-        public MyViewHolder(View v) {
-            super(v);
-            view = v;
-            textView = v.findViewById(R.id.song_title_text_view);
-        }
-
-        public void bind(String text, final OnSongClickedListener listener) {
-            textView.setText(text);
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    listener.onSongClicked();
-                }
-            });
-        }
-    }
-
-    public MyAdapter(String[] myDataset, OnSongClickedListener listener) {
-        mDataset = myDataset;
+    public MyAdapter(Context context, List<Song> songs, OnSongClickedListener listener) {
+        this.context=context;
+        this.songs = songs;
         this.listener = listener;
     }
 
@@ -48,12 +33,56 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.bind(mDataset[position], listener);
+        holder.bind(this.context,songs.get(position), listener);
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return songs.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public View view;
+        public TextView detailsTextView;
+        public ImageView imageView;
+        public TextView titleTextView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            view = v;
+            titleTextView = v.findViewById(R.id.song_title_text_view);
+            detailsTextView= v.findViewById(R.id.song_title2_text_view);
+            imageView=v.findViewById(R.id.song_image_image_view);
+        }
+
+        public void bind(Context context, final Song song, final OnSongClickedListener listener) {
+            String songName;
+            String songAuthor;
+            String[] parts = song.getName().split("-");
+
+            if (parts.length < 2) {
+                songName = parts[0];
+                songAuthor = "Unknown author";
+            } else {
+                songAuthor = parts[0];
+                songName = parts[1];
+                if (songName.startsWith(" ")) {
+                     songName = songName.substring(1);
+                }
+            }
+
+            titleTextView.setText(songName);
+            detailsTextView.setText(songAuthor);
+            Glide.with(context).load(song.getImg()).into(imageView);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.onSongClicked(song);
+                }
+            });
+        }
     }
 
     /**
@@ -61,6 +90,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
      */
     public interface OnSongClickedListener {
 
-        void onSongClicked();
+        void onSongClicked(Song song);
     }
 }
